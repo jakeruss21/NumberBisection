a38d99c7537cc466e4b45bec59804d3e01b881f4
Jacob Russell <jacob.russell82@outlook.com>
2018-10-04 21:24:38 -0400

    fixes doWhile loop in UserFindsTheNumber class to be cleaner

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -11,6 +11,7 @@ namespace GuessANumber
         static void Main(string[] args)
         {
             int target;
+            Random rand = new Random();
             UserTargetInput userInput = new UserTargetInput();
 
             int[] arrlist1 = new int[10];
@@ -25,8 +26,16 @@ namespace GuessANumber
             INumberFinder cpuFinder = new CompNumberFinder();
             cpuFinder.FindNumber(arrlist1, target);
 
+            int[] arrList2 = new int[100];
+            for(int i = 0; i < arrList2.Length; i++)
+            {
+                arrList2[i] = i + 1;
+            }
 
+            target = rand.Next(1, arrList2.Length);
 
+            UserFindsTheNumber userFinder = new UserFindsTheNumber();
+            userFinder.LookForNumber(arrList2, target);
 
         }
     }
---
+++ GuessANumber/UserFindsTheNumber.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessANumber
+{
+    class UserFindsTheNumber
+    {
+        public void LookForNumber(int[] arrList, int target)
+        {
+            int userGuess;
+            bool guessRight = false;
+            int counter = 0;
+
+            Console.WriteLine($"I have chosen a number between 1 and {arrList.Length}.  Let's see how quickly you can find it");
+            Console.WriteLine("I'll tell you if your number is higher or lower than my number.");
+
+            do
+            {
+                Console.WriteLine($"Enter a number between 1 and {arrList.Length}");
+                userGuess = UtilityHandler.ErrorHandler(arrList.Length);
+
+                guessRight = FindNumber(target, userGuess);
+                counter++;
+
+            } while (!guessRight);
+
+            Console.WriteLine($"Congratulations! You found the correct number in {counter} guesses!");
+        }
+
+
+
+        private bool FindNumber(int target, int userGuess)
+        {
+            bool guessRight;
+
+            if(userGuess < target)
+            {
+                Console.WriteLine("Your guess is too low.");
+                guessRight = false;
+            }
+            else if(userGuess > target)
+            {
+                Console.WriteLine("Your guess is too high.");
+                guessRight = false;
+            }
+            else
+            {
+                guessRight = true;
+            }
+
+            return guessRight;
+        }
+    }
+}
